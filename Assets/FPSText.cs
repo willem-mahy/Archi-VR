d2d307541050e5517cdc30ba33a8668905eeb06c
wmahy <willem.mahy@live.com>
2019-10-04 16:23:36 +0200

    Implemented HUD FPS counter

---
+++ Assets/FPSText.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FPSText : MonoBehaviour
+{
+    private Text text = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = gameObject.GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (text == null)
+        {
+            return;
+        }
+        
+        text.text = "FPS: " + (int)(1f / Time.unscaledDeltaTime);
+    }
+}
