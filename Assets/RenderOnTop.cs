ed774cb2122deca03f585753afa9622a3c55ef2a
wmahy <willem.mahy@live.com>
2019-10-07 17:21:53 +0200

    Implemented RenderOnTop behavior, used to render World-Space UI on top of everything else.

---
+++ Assets/RenderOnTop.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+public class RenderOnTop : MonoBehaviour
+{
+    public UnityEngine.Rendering.CompareFunction comparison = UnityEngine.Rendering.CompareFunction.Always;
+
+    public bool apply = false;
+
+    private void Start()
+    {
+        Apply();
+    }
+
+    private void Update()
+    {
+        if (apply)
+        {
+            Apply();
+        }
+    }
+
+    private void Apply()
+    {
+        Debug.Log("RenderOnTop.Apply()");
+
+        apply = false;
+        
+        var graphic = GetComponent<Graphic>();
+
+        // Get a handle to the existing material from the graphic.
+        var existingGlobalMat = graphic.materialForRendering;
+
+        // Make a copy of the existing material.
+        var updatedMaterial = new Material(existingGlobalMat);
+
+        // Set the Z test mode on the copy.
+        updatedMaterial.SetInt("unity_GUIZTestMode", (int)comparison);
+
+        // Set the copy material as the graphic material.
+        graphic.material = updatedMaterial;
+    }
+}
