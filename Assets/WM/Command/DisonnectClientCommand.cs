a2d21a476375e2a3e45b97de75f1ab50b6c04bf1
wmahy <willem.mahy@live.com>
2019-10-24 11:58:44 +0200

    Implemented DisconnectClient and InitNetwork Commands

---
+++ Assets/WM/Command/DisonnectClientCommand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Xml.Serialization;
+
+using UnityEngine;
+
+using WM.ArchiVR;
+
+namespace WM.ArchiVR.Command
+{
+    [Serializable]
+    [XmlRoot("DisonnectClientCommand")]
+    public class DisconnectClientCommand : ICommand
+    {
+        [XmlElement("ClientIP")]
+        public string ClientIP { get; set; }
+
+        public DisconnectClientCommand()
+        { 
+        }
+
+        public void Execute(ApplicationArchiVR application)
+        {
+            Debug.Log("DisconnectClientCommand.Execute()");
+
+            application.DisconnectClient(ClientIP);
+        }
+    }
+}
---
+++ Assets/WM/Command/InitNetworkCommand.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Xml.Serialization;
+using UnityEngine;
+
+using WM.ArchiVR;
+using WM.Net;
+
+namespace WM.ArchiVR.Command
+{
+    [Serializable]
+    [XmlRoot("InitNetworkCommand")]
+    public class InitNetworkCommand : ICommand
+    {
+        [XmlElement("NetworkMode")]
+        public NetworkMode NetworkMode { get; set; }
+
+        public InitNetworkCommand(NetworkMode networkMode)
+        {
+            NetworkMode = networkMode;
+        }
+
+        public void Execute(ApplicationArchiVR application)
+        {
+            Debug.Log("InitNetworkCommand.Execute()");
+
+            if (application.NetworkMode == NetworkMode)
+            {
+                return; // NOOP: already running in requested network mode...
+            }
+
+            if (application.Server != null)
+            {
+                application.Client.Shutdown();
+            }
+
+            if (application.Server != null)
+            {
+                application.Server.Shutdown();
+            }
+
+            switch (NetworkMode)
+            {
+                case NetworkMode.Server:
+                    {
+                        // Init network server
+                        application.Server.Init();
+
+                        // Init network client
+                        application.Client.InitialServerIP = NetUtil.GetLocalIPAddress(); // Let client connect to own server. (TODO: connect directly, ie without network middle layer.)
+                        application.Client.Init();
+                    }
+                    break;
+                case NetworkMode.Client:
+                    {
+                        // Init network client only
+                        application.Client.Init();
+                    }
+                    break;
+                case NetworkMode.Standalone:
+                    {
+                        // Init no network
+                    }
+                    break;
+            }
+
+            application.NetworkMode = NetworkMode;
+        }
+    }
+}
