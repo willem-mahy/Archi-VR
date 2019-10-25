6214ced8f530572e4e6f6fdfb912aa6e1367b1ca
wmahy <willem.mahy@live.com>
2019-10-25 17:56:28 +0200

    Removed unused using.

--- Assets/WM/Net/Client.cs
+++ Assets/WM/Net/Client.cs
@@ -5,7 +5,6 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Xml.Serialization;
 using UnityEngine;
 using WM.ArchiVR;
