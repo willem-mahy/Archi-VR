9e3dc124d0ecfabb84b1c8a567b4649233c1a06f
wmahy <willem.mahy@live.com>
2019-10-25 15:21:17 +0200

    Implemented ServerShutDownCommand

---
+++ Assets/WM/Command/ServerShutDownCommand.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Xml.Serialization;
+
+using UnityEngine;
+using WM.Net;
+
+namespace WM.ArchiVR.Command
+{
+    [Serializable]
+    [XmlRoot("ServerShutdownCommand")]
+    public class ServerShutdownCommand : ICommand
+    {
+        public ServerShutdownCommand()
+        { 
+        }
+
+        public void Execute(ApplicationArchiVR application)
+        {
+            Debug.Log("ServerShutdownCommand.Execute()");
+
+            lock (application.avatars)
+            {
+                foreach (var avatar in application.avatars.Values)
+                {
+                    GameObject.Destroy(avatar);
+                }
+
+                application.avatars.Clear();
+            }
+
+            new InitNetworkCommand(NetworkMode.Standalone).Execute(application);
+        }
+    }
+}
