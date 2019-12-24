5ee14df9930af3b02cc1b78eab1049f77df1129e
wmahy <willem.mahy@live.com>
2019-12-24 12:37:50 +0100

    Moved unit tests for UDPSend and UDPReceive to their own code file.

--- Assets/Tests/TestServerClient.cs
+++ Assets/Tests/TestServerClient.cs
@@ -1,109 +1,45 @@
 ﻿using ArchiVR.Application;
 using ArchiVR.Net;
 using NUnit.Framework;
-using System.Collections;
-using System.Net.Sockets;
 using System.Threading;
 using UnityEngine;
-using UnityEngine.TestTools;
+using WM.Command;
 using WM.Net;
 
 namespace Tests
 {
     public class TestServerClient
     {
-        // Test:
-        // - Creating a UDPReceiver and 2 UDPSender instances targeting the receiver.
-        // - Sending messages from senders to the receiver.
-        [Test]
-        public void TestUDPSendReceive()
+        /// <summary>
+        /// Create an ApplicationArchiVR with a Server and Client set.
+        /// </summary>
+        /// <returns></returns>
+        private ApplicationArchiVR CreateApplication()
         {
-            var ip = "127.0.0.1";
-            var receiverPort = 8880;
-            var sender1Port = 8881;
-            var sender2Port = 8882;
+            // Create an application instance that will act as server.
+            var applicationGO = new GameObject();
+            var application = applicationGO.AddComponent(typeof(ApplicationArchiVR)) as ApplicationArchiVR;
+
+            // Create the server for the server application.
+            var serverGO = new GameObject();
+            var server = applicationGO.AddComponent(typeof(ServerArchiVR)) as ServerArchiVR;
+            server.application = application;
+            application.Server = server;
+
+            var clientGO = new GameObject();
+            var client = clientGO.AddComponent(typeof(ClientArchiVR)) as ClientArchiVR;
+            client.application = application;
+            application.Client = client;
+
+            application.StartupNetworkMode = NetworkMode.Standalone;
+
+            application.Start();
+
+            Assert.AreEqual(0, application.Server.NumClients);
             
-            var sender1Key = UDPReceive.GetRemoteEndpointKey(ip, sender1Port);
-            var sender2Key = UDPReceive.GetRemoteEndpointKey(ip, sender2Port);
+            Assert.IsFalse(application.Client.Connected);
 
-            var hello1 = "Hello from " + sender1Port;
-            var hello2 = "Hello from " + sender2Port;
-            var bye1 = "Bye from " + sender1Port;
-            var bye2 = "Bye from " + sender2Port;
-
-            #region Setup
-
-            var receiverUdpClient = new UdpClient(receiverPort);
-            var receiver = new UDPReceive(receiverUdpClient);
-            receiver.Init();
-
-            var sender1UdpClient = new UdpClient(sender1Port);
-            var sender1 = new UDPSend(sender1UdpClient);
-            sender1.remoteIP = ip;
-            sender1.remotePort = receiverPort; 
-            sender1.Init();
-
-            var sender2UdpClient = new UdpClient(sender2Port);
-            var sender2 = new UDPSend(sender2UdpClient);
-            sender2.remoteIP = ip;
-            sender2.remotePort = receiverPort;
-            sender2.Init();
-
-            #endregion
-
-            // Check startup state.
-            Assert.AreEqual(0, receiver.allReceivedUDPPackets.Count);
-            Assert.AreEqual("", receiver.lastReceivedUDPPacket);
-
-            // Send 'Hello' message from sender 1
-            sender1.SendString(hello1);
-
-            Thread.Sleep(100);
-
-            Assert.AreEqual(1, receiver.allReceivedUDPPackets.Count);
-
-            Assert.AreEqual(hello1, receiver.allReceivedUDPPackets[sender1Key]);
-
-            Assert.AreEqual(hello1, receiver.lastReceivedUDPPacket);
-
-            // Send 'Hello' message from sender 2
-            sender2.SendString(hello2);
-
-            Thread.Sleep(100);
-            
-            Assert.AreEqual(2, receiver.allReceivedUDPPackets.Count);
-
-            Assert.AreEqual(hello2, receiver.allReceivedUDPPackets[sender2Key]);
-
-            Assert.AreEqual(hello2, receiver.lastReceivedUDPPacket);
-            
-            // Send 'Bye' message from sender 1
-            sender1.SendString(bye1);
-
-            Thread.Sleep(100);
-
-            Assert.AreEqual(hello1 + bye1, receiver.allReceivedUDPPackets[sender1Key]);
-
-            Assert.AreEqual(bye1, receiver.lastReceivedUDPPacket);
-
-            // Send 'Bye' message from sender 1
-            sender2.SendString(bye2);
-
-            Thread.Sleep(500);
-
-            Assert.AreEqual(hello2 + bye2, receiver.allReceivedUDPPackets[sender2Key]);
-
-            Assert.AreEqual(bye2, receiver.lastReceivedUDPPacket);
-            
-            #region Teardown
-
-            receiver.Shutdown();
-            
-            receiverUdpClient.Close();
-            sender1UdpClient.Close();
-            sender2UdpClient.Close();
-
-            #endregion
+            return application;
         }
 
         // Test:
@@ -114,32 +50,73 @@ namespace Tests
         [Test]
         public void TestServerClientConnect()
         {
+            #region Setup
+
             // Create an application instance that will act as server.
-            var applicationServerGO = new GameObject();
-            var applicationServer = applicationServerGO.AddComponent(typeof(ApplicationArchiVR)) as ApplicationArchiVR;
+            var applicationServer = CreateApplication();
 
-            // Create an application instance that will connect as client.
-            var applicationClientGO = new GameObject();
-            var applicationClient = applicationClientGO.AddComponent(typeof(ApplicationArchiVR)) as ApplicationArchiVR;
+            // Create an application instance that will connect as client 1.
+            var applicationClient1 = CreateApplication();
 
-            // Create the server for the server application.
-            var serverGO = new GameObject();
-            var server = applicationServerGO.AddComponent(typeof(ServerArchiVR)) as ServerArchiVR;
-            server.application = applicationServer;
+            // Create an application instance that will connect as client 2.
+            var applicationClient2 = CreateApplication();
 
-            Assert.AreEqual(0, server.NumClients);
+            #endregion
 
-            server.Init();
+            Assert.AreEqual(0, applicationServer.Server.NumClients);
+            Assert.AreEqual(0, applicationClient1.Server.NumClients); 
+            Assert.AreEqual(0, applicationClient2.Server.NumClients);
 
-            Assert.AreEqual(0, server.NumClients);
+            Assert.IsFalse(applicationServer.Client.Connected);
+            Assert.IsFalse(applicationClient1.Client.Connected);
+            Assert.IsFalse(applicationClient2.Client.Connected);
 
-            var client0GO = new GameObject();
-            var client0 = client0GO.AddComponent(typeof(ClientArchiVR)) as ClientArchiVR;
-            client0.application = applicationClient;
+            // Make server application initialize network mode from 'Standalone' to 'Server'.
+            Assert.AreEqual(NetworkMode.Standalone, applicationServer.NetworkMode);
+            applicationServer.QueueCommand(new InitNetworkCommand(NetworkMode.Server));
+            Assert.AreEqual(NetworkMode.Standalone, applicationServer.NetworkMode);
+            applicationServer.Update();
+            Assert.AreEqual(NetworkMode.Server, applicationServer.NetworkMode);
 
-            Assert.AreEqual(0, server.NumClients);
-            Assert.AreEqual(false, client0.Connected);
+            // Make client1 application initialize network mode from 'Standalone' to 'Client'.
+            Assert.AreEqual(NetworkMode.Standalone, applicationClient1.NetworkMode);
+            applicationClient1.QueueCommand(new InitNetworkCommand(NetworkMode.Client));
+            Assert.AreEqual(NetworkMode.Standalone, applicationClient1.NetworkMode);
 
+            for (int i = 0; i < 15; ++i)
+            {
+                Thread.Sleep(100);
+                applicationClient1.Update();
+                applicationClient2.Update();
+                applicationServer.Update();
+            }
+            
+
+            Assert.AreEqual(NetworkMode.Client, applicationClient1.NetworkMode);
+            Assert.IsTrue(applicationClient1.Client.Connected);
+
+            Assert.AreEqual(1, applicationServer.Server.NumClients);
+
+            // Make client2 application initialize network mode from 'Standalone' to 'Client'.
+            Assert.AreEqual(NetworkMode.Standalone, applicationClient1.NetworkMode);
+            applicationClient2.QueueCommand(new InitNetworkCommand(NetworkMode.Client));
+            Assert.AreEqual(NetworkMode.Standalone, applicationClient1.NetworkMode);
+
+            for (int i = 0; i < 15; ++i)
+            {
+                Thread.Sleep(100);
+                applicationClient1.Update();
+                applicationClient2.Update();
+                applicationServer.Update();
+            }
+
+            Assert.AreEqual(NetworkMode.Client, applicationClient2.NetworkMode);
+            Assert.IsTrue(applicationClient2.Client.Connected);
+
+            Assert.AreEqual(2, applicationServer.Server.NumClients);
+
+            /*
+            
             // Let client A connect
             client0.Init();
 
@@ -162,6 +139,7 @@ namespace Tests
             Assert.AreEqual(0, server.NumClients);
 
             server.Shutdown();
+            */
         }
 
         //// A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
---
+++ Assets/Tests/TestUDPSendReceive.cs
@@ -0,0 +1,104 @@
+﻿using NUnit.Framework;
+using System.Net.Sockets;
+using System.Threading;
+using WM.Net;
+
+namespace Tests
+{
+    public class TestUDPSendReceive
+    {
+        // Test:
+        // - Creating a UDPReceiver and 2 UDPSender instances targeting the receiver.
+        // - Sending messages from senders to the receiver.
+        [Test]
+        public void TestUDPSendReceive_ConnectAndSend()
+        {
+            var ip = "127.0.0.1";
+            var receiverPort = 8880;
+            var sender1Port = 8881;
+            var sender2Port = 8882;
+            
+            var sender1Key = UDPReceive.GetRemoteEndpointKey(ip, sender1Port);
+            var sender2Key = UDPReceive.GetRemoteEndpointKey(ip, sender2Port);
+
+            var hello1 = "Hello from " + sender1Port;
+            var hello2 = "Hello from " + sender2Port;
+            var bye1 = "Bye from " + sender1Port;
+            var bye2 = "Bye from " + sender2Port;
+
+            #region Setup
+
+            var receiverUdpClient = new UdpClient(receiverPort);
+            var receiver = new UDPReceive(receiverUdpClient);
+            receiver.Init();
+
+            var sender1UdpClient = new UdpClient(sender1Port);
+            var sender1 = new UDPSend(sender1UdpClient);
+            sender1.remoteIP = ip;
+            sender1.remotePort = receiverPort; 
+            sender1.Init();
+
+            var sender2UdpClient = new UdpClient(sender2Port);
+            var sender2 = new UDPSend(sender2UdpClient);
+            sender2.remoteIP = ip;
+            sender2.remotePort = receiverPort;
+            sender2.Init();
+
+            #endregion
+
+            // Check startup state.
+            Assert.AreEqual(0, receiver.allReceivedUDPPackets.Count);
+            Assert.AreEqual("", receiver.lastReceivedUDPPacket);
+
+            // Send 'Hello' message from sender 1
+            sender1.SendString(hello1);
+
+            Thread.Sleep(100);
+
+            Assert.AreEqual(1, receiver.allReceivedUDPPackets.Count);
+
+            Assert.AreEqual(hello1, receiver.allReceivedUDPPackets[sender1Key]);
+
+            Assert.AreEqual(hello1, receiver.lastReceivedUDPPacket);
+
+            // Send 'Hello' message from sender 2
+            sender2.SendString(hello2);
+
+            Thread.Sleep(100);
+            
+            Assert.AreEqual(2, receiver.allReceivedUDPPackets.Count);
+
+            Assert.AreEqual(hello2, receiver.allReceivedUDPPackets[sender2Key]);
+
+            Assert.AreEqual(hello2, receiver.lastReceivedUDPPacket);
+            
+            // Send 'Bye' message from sender 1
+            sender1.SendString(bye1);
+
+            Thread.Sleep(100);
+
+            Assert.AreEqual(hello1 + bye1, receiver.allReceivedUDPPackets[sender1Key]);
+
+            Assert.AreEqual(bye1, receiver.lastReceivedUDPPacket);
+
+            // Send 'Bye' message from sender 2
+            sender2.SendString(bye2);
+
+            Thread.Sleep(500);
+
+            Assert.AreEqual(hello2 + bye2, receiver.allReceivedUDPPackets[sender2Key]);
+
+            Assert.AreEqual(bye2, receiver.lastReceivedUDPPacket);
+            
+            #region Teardown
+
+            receiver.Shutdown();
+            
+            receiverUdpClient.Close();
+            sender1UdpClient.Close();
+            sender2UdpClient.Close();
+
+            #endregion
+        }
+    }
+}
