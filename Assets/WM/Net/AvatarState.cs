4cfa38f37820af84974f065a77773d44b54141b7
wmahy <willem.mahy@live.com>
2019-12-25 10:10:39 +0100

    Removed unused usings and commented-out code

--- Assets/WM/Net/AvatarState.cs
+++ Assets/WM/Net/AvatarState.cs
@@ -1,38 +1,24 @@
 ﻿
 using Assets.WM.Unity;
 using System;
-using System.Xml;
-using System.Xml.Serialization;
-using UnityEngine;
 
-namespace WM
+namespace WM.Net
 {
-    namespace Net
+    [Serializable]
+    public class AvatarState
     {
-        [Serializable]
-        //[XmlRoot("AvatarState")]
-        public class AvatarState
-        {
-            //[XmlElement("CLientIP")]
-            public string ClientIP { get; set; } = "";
+        public string ClientIP { get; set; } = "";
 
-            //[XmlElement("HeadPosition")]
-            public SerializableVector3 HeadPosition { get; set; } = new SerializableVector3();
+        public SerializableVector3 HeadPosition { get; set; } = new SerializableVector3();
 
-            //[XmlElement("HeadRotation")]
-            public SerializableQuaternion HeadRotation { get; set; } = new SerializableQuaternion();
+        public SerializableQuaternion HeadRotation { get; set; } = new SerializableQuaternion();
 
-            //[XmlElement("LHandPosition")]
-            public SerializableVector3 LHandPosition { get; set; } = new SerializableVector3();
+        public SerializableVector3 LHandPosition { get; set; } = new SerializableVector3();
 
-            //[XmlElement("LHandRotation")]
-            public SerializableQuaternion LHandRotation { get; set; } = new SerializableQuaternion();
+        public SerializableQuaternion LHandRotation { get; set; } = new SerializableQuaternion();
 
-            //[XmlElement("RHandPosition")]
-            public SerializableVector3 RHandPosition { get; set; } = new SerializableVector3();
+        public SerializableVector3 RHandPosition { get; set; } = new SerializableVector3();
 
-            //[XmlElement("RHandRotation")]
-            public SerializableQuaternion RHandRotation { get; set; } = new SerializableQuaternion();
-        }
-    } // namespace Net
-} // namespace WM
+        public SerializableQuaternion RHandRotation { get; set; } = new SerializableQuaternion();
+    }
+}
