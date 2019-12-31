773140a542ab6c729664302d3b4ec8c1c0e6bddc
wmahy <willem.mahy@live.com>
2019-12-31 11:33:30 +0100

    NetworkMode: Added a FIXME

--- Assets/WM/Net/NetworkMode.cs
+++ Assets/WM/Net/NetworkMode.cs
@@ -1,5 +1,15 @@
 ﻿namespace WM.Net
 {
+    /// <summary>
+    /// The possible network modes for an application.
+    /// - Standalone:   No networking.
+    /// - Server:       Running a Server and a Client. Client is connected to own local Server.
+    /// - Client:       Running a Client only, connected to remote Server.
+    /// 
+    /// FIXME: TODO? Split up 'Server into the following 2 states?
+    /// - Server:       Running a Server only.
+    /// - Host:         Running a Server and a Client. Client is connected to own local Server.
+    /// </summary>
     public enum NetworkMode
     {
         Standalone = 0,
