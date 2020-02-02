f9d90a3088e3cb250c5357eb3d10f78c172a6e5d
wmahy <willem.mahy@live.com>
2020-02-02 10:21:04 +0100

    Implement WM.Command.ToggleMenuModeCommand

---
+++ Assets/WM/Command/ToggleMenuModeCommand.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using WM.Application;
+
+namespace WM.Command
+{
+    [Serializable]
+    public class ToggleMenuModeCommand : ICommand
+    {
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        public ToggleMenuModeCommand()
+        { }
+
+        /// <summary>
+        /// <see cref="ICommand.Execute(UnityApplication)"/> implementation.
+        /// </summary>
+        public void Execute(UnityApplication application)
+        {
+            WM.Logger.Debug("ToggleMenuModeCommand.Execute()");
+
+            application.ToggleMenuMode();
+        }
+    }
+}
