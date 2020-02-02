a385edb5ce77741b9c8202572a4b817370f72a6a
wmahy <willem.mahy@live.com>
2020-02-02 10:21:32 +0100

    Implement WM.Command.SetMenuModeCommand

---
+++ Assets/WM/Command/SetMenuModeCommand.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using WM.Application;
+
+namespace WM.Command
+{
+    [Serializable]
+    public class SetMenuModeCommand : ICommand
+    {
+        /// <summary>
+        /// Parametrized constructor.
+        /// </summary>
+        public SetMenuModeCommand(UnityApplication.MenuMode mode)
+        {
+            _mode = mode;
+        }
+
+        /// <summary>
+        /// <see cref="ICommand.Execute(UnityApplication)"/> implementation.
+        /// </summary>
+        public void Execute(UnityApplication application)
+        {
+            WM.Logger.Debug("SetMenuModeCommand.Execute()");
+
+            application.SetMenuMode(_mode);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private readonly UnityApplication.MenuMode _mode;
+    }
+}
