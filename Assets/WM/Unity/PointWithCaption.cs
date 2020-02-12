fe78c71df9499b81a5dc73dada8eb4d7839159d8
wmahy <willem.mahy@live.com>
2020-02-12 22:38:04 +0100

    Implement behaviors for Point and ReferenceSystem prefabs.

---
+++ Assets/WM/Unity/PointWithCaption.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+namespace WM
+{
+    public class PointWithCaption : MonoBehaviour
+    {
+        public void SetText(string text)
+        {
+            this.text.text = text;
+        }
+
+        public TextMeshPro text;
+    }
+}
---
+++ Assets/WM/Unity/ReferenceSystem6DOF.cs
@@ -0,0 +1,15 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace WM
+{
+    public class ReferenceSystem6DOF : MonoBehaviour
+    {
+        public void SetText(string text)
+        {
+            this.text.text = text;
+        }
+
+        public TextMeshPro text;
+    }
+}
