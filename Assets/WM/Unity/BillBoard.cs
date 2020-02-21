ec4a910b88be5294af0a69aabc9692e6770f4518
wmahy <willem.mahy@live.com>
2020-02-21 18:11:41 +0100

    Moved BillBoard script to WM.Unity namespace.

---
+++ Assets/WM/Unity/BillBoard.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace WM.Unity
+{
+    /// <summary>
+    /// Add this as a component to a GameObject, in order to make it be oriented to the camera always.
+    /// This is done by rotating the parent GameObject around its position.
+    /// </summary>
+    public class BillBoard : MonoBehaviour
+    {
+        /// <summary>
+        /// Update is called once per frame
+        /// </summary>
+        void Update()
+        {
+            // look at camera...
+            transform.LookAt(Camera.main.transform.position, Vector3.up);
+        }
+    }
+}
