72519d0e51d5a81c155be6bc48cd10a04e69477e
wmahy <willem.mahy@live.com>
2020-02-28 09:46:43 +0100

    Improved documentation

--- Assets/WM/Tracking/Teleport/TeleportFader.cs
+++ Assets/WM/Tracking/Teleport/TeleportFader.cs
@@ -3,15 +3,24 @@ using UnityEngine;
 
 namespace WM.Tracking.Teleport
 {
+    /// <summary>
+    /// Note: although not referenced according to Intellisense, this is used via Unity editor setup.
+    /// </summary>
     public class TeleportFader : MonoBehaviour
     {
         public UnityApplication application;
 
+        /// <summary>
+        /// Note: although not referenced according to Intellisense, this is used via Unity editor setup.
+        /// </summary>
         public void OnFadeOutComplete()
         {
             application.OnTeleportFadeOutComplete();
         }
 
+        /// <summary>
+        /// Note: although not referenced according to Intellisense, this is used via Unity editor setup.
+        /// </summary>
         public void OnFadeInComplete()
         {
             application.OnTeleportFadeInComplete();
