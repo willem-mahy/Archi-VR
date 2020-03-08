f3e3a0e1a1b374be033cec76876f78fff8978545
wmahy <willem.mahy@live.com>
2020-03-08 10:23:38 +0100

    Only show containing layer while R thumb is being pressed.

--- Assets/ArchiVR/Application/ApplicationStateWalkthrough.cs
+++ Assets/ArchiVR/Application/ApplicationStateWalkthrough.cs
@@ -338,13 +338,16 @@ namespace ArchiVR
 
             var isEditor = UnityEngine.Application.isEditor;
 
-            // Update left controller UI displaying the project name.
+            // Update left controller text, to displaying the project name.
             m_application.m_leftControllerText.text = (m_application.ActiveProjectName != null) ? m_application.GetProjectName(m_application.ActiveProjectName) : "No project loaded.";
 
-
-            m_application.m_rightControllerText.text =
-                //m_application.ActivePOIName ?? "";
-                m_application.EstimateLayer(m_application.m_centerEyeAnchor);
+            // Update right controller text, to display:
+            // - by default: the POI name
+            // - while the R thumb is pressed: the estimated containing layer for the user head. (for debugging purposes)
+            var showContainingLayer = m_application.m_controllerInput.m_controllerState.rThumbstickPressed;
+            m_application.m_rightControllerText.text = showContainingLayer
+                ? m_application.EstimateLayer(m_application.m_centerEyeAnchor)
+                : m_application.ActivePOIName ?? "";
 
             // Left controller
             {
